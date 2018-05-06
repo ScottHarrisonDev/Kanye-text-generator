6c6f1dca142f93ca8796cf9c9a1bce08f5086ef2
ScottHarrisonDev <github@scottharrisondesign.co.uk>
2018-05-06 20:17:31 +0100

    styling updates and UI additions

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
+// fetch('sources/kanye-west/kanye-west-yeezus-lyrics.json')
+//     .then(blob => blob.json())
+//     .then(data => process(data['kanye-west-yeezus-lyrics']));
 fetch('sources/kanye-west/kanye-west-the-college-dropout-lyrics.json')
     .then(blob => blob.json())
     .then(data => process(data['kanye-west-the-college-dropout-lyrics']));
 
+const order = 5;
+const ngrams = [];
+let firstNgram = '';
 
 function process(source) {
-    const order = 6;
-    const ngrams = [];
 
     // Learn
     for (let i = 0; i < source.length - order; i++) {
         const ngram = source.substr(i, order);
+        if (i === 0) {
+            firstNgram = ngram;
+        }
         if (!ngrams[ngram]) {
             ngrams[ngram] = [];
         }
         const nextChar = source.substr(i + order, 1);
         ngrams[ngram].push(nextChar);
     }
+}
 
-    // Generate
-    const start = 40;
-    // let output = source.substring(start, start + order); // hardcoded to use the same first word for now
-    let output = 'I wanna';
+function generate() {
+    let output = firstNgram;
     for (let i = 0; i < 500; i++) {
         const current = output.substring(output.length - order);
         const possibilities = ngrams[current];
         output += possibilities[Math.floor(Math.random() * possibilities.length)];
     }
-    console.log(output);
+    document.getElementById('generated-text').innerHTML = output;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('generate').addEventListener('click', generate);
+});
